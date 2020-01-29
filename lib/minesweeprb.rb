c6ac58e7a23555756c81875ef1e8c7c61979d631
Adam Anderson <3806+scudco@users.noreply.github.com>
2020-01-28 22:55:53 -0500

    Initial commit

---
+++ lib/minesweeprb.rb
@@ -0,0 +1,6 @@
+require "minesweeprb/version"
+
+module Minesweeprb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/minesweeprb/version.rb
@@ -0,0 +1,3 @@
+module Minesweeprb
+  VERSION = "0.1.0"
+end
